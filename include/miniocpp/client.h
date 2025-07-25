6da9f7b844edcf48c76cc0c4213ee3c5153753cc
Ширяев Евгений Анатольевич <shiryaev.evgeniy@rt.ru>
2025-07-25 10:23:00 +0300

    Bugfix. witg_logging flag added to Client.

--- include/miniocpp/client.h
+++ include/miniocpp/client.h
@@ -115,7 +115,7 @@ class Client : public BaseClient {
                               char* buf);                              
 
  public:
-  explicit Client(BaseUrl& base_url, creds::Provider* const provider = nullptr, with_loggin = false);
+  explicit Client(BaseUrl& base_url, creds::Provider* const provider = nullptr, const bool loggin = false);
   ~Client() = default;
 
   ComposeObjectResponse ComposeObject(ComposeObjectArgs args);
