1d10f029eac80bc5b2b3ae4fba86798a9c954e44
Александр Елизаров <aleksandr.elizarov@rt.ru>
2025-11-25 16:06:35 +0300

    Revert "DEBUG: exlude curlpp"
    This reverts commit 17f22b83211f1fa256c5bd5c7f11b1ed9ffcbb3b.

--- src/utils.cc
+++ src/utils.cc
@@ -212,8 +212,7 @@ std::string EncodePath(const std::string& path) {
   while (std::getline(str_stream, token, '/')) {
     if (!token.empty()) {
       if (!out.empty()) out += "/";
-      //out += curlpp::escape(token);
-      out += token;
+      out += curlpp::escape(token);
     }
   }
 
